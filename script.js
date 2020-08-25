2c6088e8105d1c3c48e0ad66b3ef236bb7d821b5
Shirobachi <szevaa97@gmail.com>
2020-08-25 04:31:06 +0200

    Added icon to mail and login page
    - on bottom left added icon to login page
    - on bottom right added icon to mailing me

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-window.onload = start;
+//TODO
+/*
+	start with random hobby/lang and increment index 
+	Changing separatelly not both at the same time
+*/
+
+var colors = ['b2abf2ff', 'c3423fff', 'f7b538ff', '61e294ff', '75ddddff', 'ffbfa0ff', '18f2b2ff', 'f5f749ff', 'f24236ff', 'AA4465', 'FFA69E', '007991', 'A14A76', 'A1E8AF'];
+
+var colorSize = colors.length; 
+var colorRandId = Math.floor(Math.random() * Math.floor(colorSize));
+
+$( ".color" ).css( "color", "#"+colors[colorRandId] );
+$( "a .floatingIcon" ).css( "color", "#"+colors[colorRandId] );
+$( '.floatingIcon' ).css( 'border', '1px solid #'+colors[colorRandId] );
+
+setTimeout(switcher, Math.floor(Math.random() * Math.floor(200)), "hobby");
+setTimeout(switcher, Math.floor(Math.random() * Math.floor(200)), "lang");
 
 var lang = ["c++", "pytnon", "html", "css", "js", "php", "sql", "regex", "latex", "python"];
 var hobby = ["git", "sublime text", "colorful things", "linux", "skateboards", "ukulele", "Rubic's cube", "Mathematic", "Instagram"];
@@ -8,11 +24,13 @@ function switcher (type){
 		var langSize = lang.length; 
 		var langRandId = Math.floor(Math.random() * Math.floor(langSize));
 
-		$( '#codeLang').removeClass('animate');
 		$( '#codeLang').html(lang[langRandId]);
+
+		$( '#codeLang').removeClass('animate');
+	  void document.getElementById("codeLang").offsetWidth;
 		$( '#codeLang').addClass('animate');
 
-		setTimeout(switcher, 1500, "lang");
+		setTimeout(switcher, Math.floor(Math.random() * Math.floor(889))+3556, "lang");
 	}
 	else{
 		var hobbySize = hobby.length; 
@@ -20,20 +38,10 @@ function switcher (type){
 
 		$( '#hobbies').html(hobby[hobbyRandId]);
 
-		setTimeout(switcher, Math.floor(Math.random() * Math.floor(500))+1500, "hobby");
+		$( '#hobbies').removeClass('animate');
+	  void document.getElementById("hobbies").offsetWidth;
+		$( '#hobbies').addClass('animate');
+
+		setTimeout(switcher, Math.floor(Math.random() * Math.floor(1500))+4000, "hobby");
 	}
-}
-
-function start () {
-
-	var colors = ['b2abf2ff', 'c3423fff', 'f7b538ff', '61e294ff', '75ddddff', 'ffbfa0ff', '18f2b2ff', 'f5f749ff', 'f24236ff', 'AA4465', 'FFA69E', '007991', 'A14A76', 'A1E8AF'];
-
-	var colorSize = colors.length; 
-	var colorRandId = Math.floor(Math.random() * Math.floor(colorSize));
-
-	$( ".color" ).css( "color", "#"+colors[colorRandId] );
-
-	switcher("lang");
-	switcher("hobby");
-
 }
