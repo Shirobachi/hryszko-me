8a93a18d3ce5f990c1d15f05fb3aaa2008be178b
Simon Hryszko <simon@hryszko.dev>
2023-07-20 21:29:53 +0200

    Add primary colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        text: '#e1fcff',
+        background: '#001719',
+        primary: '#9af6fe',
+        secondary: '#002023',
+        accent: '#02daed',
+      },
+    },
   },
   plugins: [],
 }
