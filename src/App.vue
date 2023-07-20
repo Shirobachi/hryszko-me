1189cc714b46586cd67c1b5a022ef6a1be4bc709
Simon Hryszko <simon@hryszko.dev>
2023-07-20 19:44:08 +0200

    Install tailwindcss

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,10 @@
     <div class="wrapper">
       <HelloWorld msg="You did it!" />
 
+      <h1 class="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
