3e62bca697af1500462bb7d82933f75713d5a157
Simon Hryszko <simon@hryszko.dev>
2023-07-21 20:34:06 +0200

    Make debug space TODEL

---
+++ src/views/DebugView.vue
@@ -0,0 +1,16 @@
+<script setup>
+  import GraphImageComponent from '../components/GraprhImageComponent.vue'
+  import axios from 'axios'
+
+  const type = 'GET';
+  const url = 'https://wakatime.com/share/@b74ba3c5-2883-43ca-9833-799f8a50840a/9724ef06-7081-4a0b-905a-c5f6e986e756.json';
+
+  const data = axios.get(url);
+</script>
+
+<template>
+  <main>
+    <!-- <GraphImageComponent /> -->
+    <!-- <GraphImageComponent side="left" /> -->
+  </main>
+</template>
