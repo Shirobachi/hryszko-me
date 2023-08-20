fecd3ce5edc06ea0fd4729fcd1f09d457e4a9644
Simon Hryszko <simon@hryszko.dev>
2023-08-20 02:18:33 +0200

    Add chart component to visualize api data in homeview

---
+++ src/components/Chart.vue
@@ -0,0 +1,52 @@
+<template>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
+  </div>
+</template>
+
+<script>
+  import { Bar } from 'vue-chartjs'
+  import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+  import axios from 'axios'
+
+  ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+  export default {
+    name: 'BarChart',
+    components: { Bar },
+    props: {
+      api_url: {
+        type: String,
+        required: true,
+      },
+    },
+    data: () => ({
+      loaded: false,
+      chartData: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Coding time',
+            data: [],
+          },
+        ],
+      },
+    }),
+    async mounted () {
+      this.loaded = false
+
+      try {
+        const { data } = await axios.get(this.api_url)
+        
+        for (const day of data.data) {
+          this.chartData.labels.push(day.range.start.substring(0, 10))
+          this.chartData.datasets[0].data.push(day.grand_total.total_seconds / 3600)
+        }
+
+        this.loaded = true
+      } catch (e) {
+        console.error(e)
+      }
+    },
+  }
+</script>
